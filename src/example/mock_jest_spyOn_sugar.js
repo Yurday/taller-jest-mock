d8c6b1be6c008e1186cf7a5dc1cd34979536d4c3
Cristhian <yurday93@hotmail.com>
2021-06-27 14:52:45 -0500

    Análisis del test mock_jest_spyOn_sugar.js

--- src/example/mock_jest_spyOn_sugar.js
+++ src/example/mock_jest_spyOn_sugar.js
@@ -1,18 +1,21 @@
 import * as app from "./app";
 import * as math from "./math";
 test("calls math.add", () => {
-  // store the original implementation
+  // Accede al método original
   const originalAdd = math.add;
-  // mock add with the original implementation
+  // Mockea la copia del método
   math.add = jest.fn(originalAdd);
-  // spy the calls to add
+  // Testea el método doAdd de app
   expect(app.doAdd(1, 2)).toEqual(3);
   expect(math.add).toHaveBeenCalledWith(1, 2);
-  // override the implementation
+  // Espía el llamado al método
   math.add.mockImplementation(() => "mock");
+  // Sobreescribe el método mock
   expect(app.doAdd(1, 2)).toEqual("mock");
+  // Evalúa el retorno "mock" del método sobreescrito
   expect(math.add).toHaveBeenCalledWith(1, 2);
-  // restore the original implementation
+  // Vuelve a la implementación original
   math.add = originalAdd;
+  // Evalúa la respuesta del método original
   expect(app.doAdd(1, 2)).toEqual(3);
 });
